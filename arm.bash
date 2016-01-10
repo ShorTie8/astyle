87f18c27a4e1cd9838c5e76b39ba17edc660932f
ShorTie8 <idiot@dot.com>
2016-01-10 02:59:47 +0000

    Helps sometimes to set JAVA_HOME

--- arm.bash
+++ arm.bash
@@ -4,7 +4,7 @@ cd astyle-code/AStyle/build/gcc/
 
 make clean
 
-CFLAGS="-Os" LDFLAGS="-s" make java
+JAVA_HOME=`readlink -f /usr/bin/javac | sed "s:bin/javac::"` CFLAGS="-Os" LDFLAGS="-s" make java
 
 cp bin/libastyle*.so ../../../../libastylej_arm.so
 
